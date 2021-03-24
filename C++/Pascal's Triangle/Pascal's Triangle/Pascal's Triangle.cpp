9142f20ead705d0f8e07660b227399fba8c8f24e
sdy362 <seodye04@gmail.com>
2021-03-25 00:40:34 +0900

    기능추가: 파스칼의 삼각형 프로그램 추가

---
+++ C++/Pascal's Triangle/Pascal's Triangle/Pascal's Triangle.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int num = 10;
+
+    int a[num][num] = { 0, };
+    int i = 0;
+    int j = 0;
+
+    for (i = 0; i < num; i++)
+    {
+        a[i][0] = 1;
+    }
+
+    for (i = 0; i < num; i++)
+    {
+        for (j = 0; j < num; j++)
+        {
+            if (i > 0 && j > 0)
+            {
+                a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
+            }
+
+            if (a[i][j] > 0)
+            {
+                cout << a[i][j] << " ";
+            }
+        }
+
+        cout << endl;
+    }
+}
