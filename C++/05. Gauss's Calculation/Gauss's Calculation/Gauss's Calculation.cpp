8102d7f447fd0ce481f0972988c4e9a6b065a7aa
sdy362 <seodye04@gmail.com>
2021-03-26 18:58:49 +0900

    기능추가: 가우스의 계산법으로 연속한 수를 더하는 프로그램 추가

---
+++ C++/05. Gauss's Calculation/Gauss's Calculation/Gauss's Calculation.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned long long num;
+    unsigned long long total = 0;
+
+    cout << "1부터 4294967295까지의 수 중에서 1개를 입력해 주세요: ";
+    cin >> num;
+    cout << endl;
+
+    if (num < 1 || num > 4294967295)
+    {
+        cout << "잘못된 숫자 입니다." << endl;
+        return 0;
+    }
+
+    total = ((1 + num) * num) / 2;
+
+    cout << "1부터 " << num << "까지 연속한 수를 모두 더한 값은 " << total << "입니다" << endl;
+}
