72ff362ffd438bfe48271917cca985f0757faa3d
sdy362 <seodye04@gmail.com>
2021-03-31 00:11:09 +0900

    기능추가: 피보나치수열을 출력하는 프로그램 추가

---
+++ C++/07. FibonacciSequence/FibonacciSequence/FibonacciSequence.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int calculation(int n)
+{
+    if (n == 1 || n == 2)
+        return 1;
+
+    return(calculation(n - 1) + calculation(n - 2));
+}
+
+int main()
+{
+    int i;
+    cout << "피보나치 수열 구현하기" << endl;
+    for (i = 1; i <= 30; i++)
+        cout << i << ": " << calculation(i) << endl;
+}
