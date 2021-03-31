9e7cac787bd2ccb0c97a2500117c79a9e3ce7a33
sdy362 <seodye04@gmail.com>
2021-03-31 17:35:01 +0900

    기능추가: 구구단을 출력하는 프로그램 추가

---
+++ C++/10. MultiplicationTable/MultiplicationTable/MultiplicationTable.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+void Multiplication(int num = 1, int num2 = 1)
+{
+    cout << num << " X " << num2 << " = " << num * num2 << endl;
+
+    if (num == 9 && num2 == 9)
+        return;
+
+    if (num2 == 9)
+    {
+        num++;
+        num2 = 1;
+        cout << endl;
+    }
+    else
+    {
+        num2++;
+    }
+
+    return(Multiplication(num, num2));
+}
+
+int main()
+{
+    Multiplication();
+}
