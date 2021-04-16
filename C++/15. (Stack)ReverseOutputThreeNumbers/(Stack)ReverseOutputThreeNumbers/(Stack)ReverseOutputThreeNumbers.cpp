a07fd1da37413ef643d0e0c5be14112fcec95788
sdy362 <seodye04@gmail.com>
2021-04-16 23:39:48 +0900

    기능추가: 3개의 숫자를 입력받은 순서의 반대로 출력하는 프로그램

---
+++ C++/15. (Stack)ReverseOutputThreeNumbers/(Stack)ReverseOutputThreeNumbers/(Stack)ReverseOutputThreeNumbers.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	long long num[3];
+	stack<long long> s;
+
+	cout << "세 정수 거꾸로 출력하기 (스택 사용)" << endl;
+
+	cout << "-920경 이상 920경 이하의 숫자를 입력해주세요" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << i + 1 << "번째 숫자 입력: ";
+		cin >> num[i];
+
+		if (cin.fail() || num[i] < -9200000000000000000 || num[i] > 9200000000000000000)
+		{
+			cout << "잘못된 숫자 입니다." << endl;
+			return 0;
+		}
+
+		s.push(num[i]);
+	}
+
+	while (!s.empty())
+	{
+		cout << s.top() << " ";
+		s.pop();
+	}
+}
