09c3af43fb87e18da66989ba2b29ec6eda5fccfd
sdy362 <seodye04@gmail.com>
2021-04-18 00:47:52 +0900

    기능추가: 스택을 사용해서 숫자를 거꾸로 출력하는 프로그램 제작

---
+++ C++/16. (Stack)ReverseNumberOutput/(Stack)ReverseNumberOutput/(Stack)ReverseNumberOutput.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	stack<long long> s;
+
+	long long num;
+	long long digit = 1;
+
+	cout << "숫자 거꾸로 출력하기(스택 사용)" << endl;
+	cout << "0 이상 100경 미만의 숫자를 입력해주세요: ";
+	cin >> num;
+
+	if (cin.fail() || num >= 1000000000000000000)
+	{
+		cout << "잘못된 숫자 입니다." << endl;
+		return 0;
+	}
+
+	while (true)
+	{
+		if (num - digit < 0)
+		{
+			digit /= 10;
+			break;
+		}
+
+		digit *= 10;
+	}
+
+	for (long long i = 1; i <= digit; i *= 10)
+	{
+		s.push(num / (digit / i));
+		num -= num / (digit / i) * (digit / i);
+	}
+
+	while (!s.empty())
+	{
+		cout << s.top();
+		s.pop();
+	}
+}
