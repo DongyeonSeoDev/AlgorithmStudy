608d38e9fc3fb1907fdb2d0fd33b1e0850ca9f43
sdy362 <seodye04@gmail.com>
2021-05-07 13:45:49 +0900

    기능추가: 스택을 사용하여 데이터를 입력받은 순서의 반대로 출력하는 기능 추가

---
+++ C++/21. (Stack)OutputDataInReverseOrderOfInput/(Stack)OutputDataInReverseOrderOfInput/(Stack)OutputDataInReverseOrderOfInput.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+string inputData;
+stack<string> dataStack;
+
+int main()
+{
+	int count;
+
+	cout << "데이터의 갯수를 입력해 주세요: ";
+	cin >> count;
+
+	if (cin.fail() || count > 1000 || count < 1)
+	{
+		cout << "잘못된 입력 입니다." << endl;
+		return 0;
+	}
+
+	cin.ignore();
+	for (int i = 0; i < count; i++)
+	{
+		cout << i + 1 << "번째 데이터를 입력해 주세요: ";
+		getline(cin, inputData);
+		dataStack.push(inputData);
+	}
+
+	cout << endl;
+
+	while (!dataStack.empty())
+	{
+		cout << dataStack.top() << endl;
+		dataStack.pop();
+	}
+}
